1641beb14e3b5afe6bdc2c584b3a4c34f24b24e3
Souher <souher.hassan0@gmail.com>
2022-03-29 03:59:46 +0100

    creating terraform ci/cd pipeline with remote tf state

--- remote_tf_state/main.tf
+++ remote_tf_state/main.tf
@@ -11,31 +11,31 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-# resource "aws_s3_bucket" "state-file" {
-#   bucket = "souher-remote-tf-state"
+resource "aws_s3_bucket" "state-file" {
+  bucket = "souher-remote-tf-state"
   
-#   versioning {
-#     enabled = true
-#   }
+  versioning {
+    enabled = true
+  }
 
-#   server_side_encryption_configuration {
-#     rule {
-#       apply_server_side_encryption_by_default {
-#         sse_algorithm = "AES256"
-#       }
-#     }
-#   }
-# }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
 
-# resource "aws_dynamodb_table" "lock-state" {
-#   name         = "souher-remote-tf-state-lock"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "lock-state" {
+  name         = "souher-remote-tf-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
 
 
 terraform {
